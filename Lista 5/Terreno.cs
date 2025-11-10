67e8049ddf674ccb17228cc8f19acee5bf933a85
vitvitfroz <maria.froz@aluno.fiema.org.br>
2025-11-10 15:13:59 -0300

    Update Terreno.cs

--- Lista 5/Terreno.cs
+++ Lista 5/Terreno.cs
@@ -1,1 +1,20 @@
+using System;
 
+class Program {
+    static void Main() {
+        Console.Write("Informe a largura do terreno: ");
+        double largura = double.Parse(Console.ReadLine());
+
+        Console.Write("Informe o comprimento do terreno: ");
+        double comprimento = double.Parse(Console.ReadLine());
+
+        Console.Write("Informe o valor do metro quadrado: ");
+        double valorMetro = double.Parse(Console.ReadLine());
+
+        double area = largura * comprimento;
+        double preco = area * valorMetro;
+
+        Console.WriteLine("Área do terreno = " + area.ToString("F2"));
+        Console.WriteLine("Preço do terreno = R$ " + preco.ToString("F2"));
+    }
+}
